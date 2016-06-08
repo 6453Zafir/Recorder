8d8b6ac7e23ca3630eb42b7caecffa29cbd76017
6453Zafir <511599737@qq.com>
2016-06-08 10:49:54 +0800

    friend

--- Recorder_APP/app/src/main/java/com/tongji/android/recorder_app/Fragment/FriendListFragment.java
+++ Recorder_APP/app/src/main/java/com/tongji/android/recorder_app/Fragment/FriendListFragment.java
@@ -1,13 +1,29 @@
 package com.tongji.android.recorder_app.Fragment;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.tongji.android.recorder_app.Activity.ItemDetailActivity;
+import com.tongji.android.recorder_app.Activity.ItemListActivity;
+import com.tongji.android.recorder_app.Activity.dummy.DummyContent;
 import com.tongji.android.recorder_app.R;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,11 +35,16 @@ public class FriendListFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private Button SerachFriendBtn;
+    private EditText FriendPhoneNum;
+    private AlertDialog alert = null;
+    private AlertDialog.Builder builder = null;
+//    private Context mContext;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private boolean isFriendExist=false;
+    private String phoneNum;
 
     public FriendListFragment() {
         // Required empty public constructor
@@ -54,13 +75,147 @@ public class FriendListFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.friend_list_fragment, container, false);
+        View v =  inflater.inflate(R.layout.friend_list_fragment, container, false);
+        FriendPhoneNum=(EditText)v.findViewById(R.id.friend_number);
+        SerachFriendBtn = (Button)v.findViewById(R.id.friend_search);
+        if(phoneNum!=null){
+            // call the ifFriendExist function
+            if(true){
+
+            }
+        }
+        SerachFriendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                phoneNum = FriendPhoneNum.getText().toString();
+                if(!phoneNum.isEmpty()){
+                    // call the ifFriendExist function
+                    if (isFriendExist){
+                        builder = new AlertDialog.Builder(getActivity());
+                        alert = builder.setIcon(R.drawable.success)
+                                .setTitle("Adding Friends")
+                                .setMessage("Are you sure you want to add "+phoneNum)
+                                .setNegativeButton("Cancel",new DialogInterface.OnClickListener(){
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                })
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                }).create();
+                        alert.show();
+                    }else {
+                        builder = new AlertDialog.Builder(getActivity());
+                        alert = builder.setIcon(R.drawable.error)
+                                .setTitle("Sorry：")
+                                .setMessage("he phone number "+phoneNum+" haven't register")
+
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                }).create();
+                        alert.show();
+                    }
+                }else{
+                    builder = new AlertDialog.Builder(getActivity());
+                    alert = builder.setIcon(R.drawable.error)
+                            .setTitle("Sorry：")
+                            .setMessage("You must input a phone number to search")
+
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            }).create();
+                    alert.show();
+                }
+            }
+        });
+
+
+
+//        FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        View recyclerView = v.findViewById(R.id.item_friend_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+        return v;
     }
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
+    }
+
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<DummyContent.DummyItem> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_friend_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mIdView.setText(mValues.get(position).id);
+            holder.mContentView.setText(mValues.get(position).content);
+            holder.mScoreView.setText(mValues.get(position).);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mIdView;
+            public final TextView mContentView;
+            public final TextView mScoreView;
+            public DummyContent.DummyItem mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mIdView = (TextView) view.findViewById(R.id.id);
+                mContentView = (TextView) view.findViewById(R.id.content);
+                mScoreView = (TextView)view.findViewById(R.id.score);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
+
+    }
+
 
 }
