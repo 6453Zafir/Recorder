e8fb4f1fe496382fb8eb94bc38818d5f6d6a07e3
李时刚 <454281389@qq.com>
2016-06-12 19:08:21 +0800

    用户自定义习惯界面

--- Recorder_APP/app/src/main/java/com/tongji/android/recorder_app/Fragment/Punchcard.java
+++ Recorder_APP/app/src/main/java/com/tongji/android/recorder_app/Fragment/Punchcard.java
@@ -11,13 +11,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.view.menu.ExpandedMenuView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ExpandableListView;
 import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +33,10 @@ import com.tongji.android.recorder_app.Model.HabitList;
 import com.tongji.android.recorder_app.Model.SystemDefaultHabit;
 import com.tongji.android.recorder_app.Model.SystemHabitList;
 import com.tongji.android.recorder_app.R;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -41,6 +52,10 @@ public class Punchcard extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private String spinnerItemString;
+    private List<Habit> temp;
+
 
 
     public Punchcard() {
@@ -72,6 +87,7 @@ public class Punchcard extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        temp = new ArrayList<Habit>();
     }
 
     @Override
@@ -81,8 +97,6 @@ public class Punchcard extends Fragment {
         if (!SystemHabitList.flag) {
             SystemHabitList.initList();
         }
-
-
 
         View view = inflater.inflate(R.layout.punch_card_fragment, container, false);
 
@@ -104,7 +118,7 @@ public class Punchcard extends Fragment {
                 final DialogGridViewAdapter dialogGridViewAdapter = new DialogGridViewAdapter(getActivity());
                 gridView.setAdapter(dialogGridViewAdapter);
 
-                TextView textView = (TextView) dialogView.findViewById(R.id.add_custom_dialog_textView);
+                final TextView textView = (TextView) dialogView.findViewById(R.id.add_custom_dialog_textView);
 
                 if (SystemHabitList.ITEMS.size() == 0) {
                     textView.setBackgroundColor(Color.parseColor("#ee0000"));
@@ -114,24 +128,145 @@ public class Punchcard extends Fragment {
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        button.setTextColor(Color.parseColor("#ffffff"));
                         AlertDialog.Builder buttonBuilder = new AlertDialog.Builder(getActivity());
 
+                        View addOwnHabitView = inflater.inflate(R.layout.add_user_own_habit,null);
+
+
+                        final EditText editText = (EditText) addOwnHabitView.findViewById(R.id.add_user_own_habit_EditText);
+                        final Spinner spinner = (Spinner)
+                                addOwnHabitView.findViewById(R.id.add_user_own_habit_spinner);
+
+                        ArrayAdapter<CharSequence> arrayAdapter =
+                                ArrayAdapter.createFromResource(
+                                        getActivity(),R.array.select_user_habit_type,android.R.layout.simple_spinner_item
+                                );
+
+                        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                        spinner.setAdapter(arrayAdapter);
+
+                        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                spinnerItemString = (String) parent.getItemAtPosition(position);
+                            }
+
+                            @Override
+                            public void onNothingSelected(AdapterView<?> parent) {
+
+                            }
+                        });
+
+                        buttonBuilder.setView(addOwnHabitView).setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                CharSequence charSequence = editText.getText();
+
+                                if (charSequence.toString().trim().equals("")) {
+                                    AlertDialog.Builder ifNullBuilder = new AlertDialog.Builder(getActivity());
+                                    ifNullBuilder.setTitle("You must enter habit name")
+                                            .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                        }
+                                    }).create().show();
+
+                                } else {
+                                    Habit currentHabit = null;
+                                    switch (spinnerItemString) {
+                                        case "Date" :
+                                            currentHabit = new Habit("0", charSequence.toString(), 0, 0);      //记得修改ID
+                                            break;
+                                        case "Degree" :
+                                            currentHabit = new Habit("0", charSequence.toString(), 0, 1);      //记得修改ID
+                                            break;
+                                        case "Do or Not" :
+                                            currentHabit = new Habit("0", charSequence.toString(), 0, 2);      //记得修改ID
+                                            break;
+                                        case "Duration" :
+                                            currentHabit = new Habit("0", charSequence.toString(), 0, 3);      //记得修改ID
+                                            break;
+                                        default :
+                                            break;
+                                    }
+
+                                    boolean currentFlag = false;
+                                    for (int i = 0; i < HabitList.ITEMS.size(); i++) {
+
+                                        if (HabitList.ITEMS.get(i).habitName.equals(charSequence.toString().trim())) {
+                                            currentFlag = true;
+                                            break;
+                                        }
+                                    }
+
+
+                                    if (currentFlag) {
+                                        AlertDialog.Builder habitBuilder = new AlertDialog.Builder(getActivity());
+                                        habitBuilder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.dismiss();
+                                            }
+                                        }).setTitle("Habit already exists!").create().show();
+
+                                    } else {
+                                        HabitList.addItem(currentHabit);
+                                        //通知服务器并且加入到本地数据库里面，往下写
+                                    }
+
+                                    dialog.dismiss();
+                                }
+                            }
+                        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }).setNeutralButton("Reset", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                editText.setText("");
+                            }
+                        });
+
+                        buttonBuilder.create().show();
                     }
                 });
 
                 builder.setView(dialogView).setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        if(temp!=null){
+                            for(int i=0;i<temp.size();i++){
+                                HabitList.addItem(temp.get(i));
+                            }
+                        }
+                        temp.clear();
 
                         dialogGridViewAdapter.notifyDataSetChanged();
                         puncuGridViewAdapter.notifyDataSetChanged();
                         dialog.dismiss();
                         Toast.makeText(getActivity(),"Check OK",Toast.LENGTH_SHORT).show();
+                        //把系统默认的习惯加入到HabitList里面，并且更新服务器数据和本地数据库，往下写
+
+
                     }
                 }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        if(temp!=null){
+                            for(int i=0;i<temp.size();i++){
+                                SystemDefaultHabit currentHabit = new
+                                        SystemDefaultHabit(temp.get(i).id,temp.get(i).habitName,temp.get(i).score,temp.get(i).type);
+                                SystemHabitList.addItem(currentHabit);
+                            }
+                        }
+                        temp.clear();
+
                         dialog.dismiss();
                         Toast.makeText(getActivity(),"Check Cancel",Toast.LENGTH_SHORT).show();
                     }
@@ -184,11 +319,11 @@ public class Punchcard extends Fragment {
                 @Override
                 public void onClick(View v) {
                     if (checkBox.isChecked()) {
-                        HabitList.addItem(habit);
+                        //HabitList.addItem(habit);
+                        temp.add(habit);
                         SystemHabitList.removeItem(currentHabit);
                         System.out.println("Habit : " + HabitList.ITEMS.size() + ", System: " + SystemHabitList.ITEMS.size());
                     } else {
-
 
                         if (HabitList.ITEMS.remove(habit)) {
                             System.out.println("Successfully");
@@ -247,5 +382,6 @@ public class Punchcard extends Fragment {
             return convertView;
         }
     }
+
 
 }
